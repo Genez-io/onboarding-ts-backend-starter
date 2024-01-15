417e76b45d544a7d589281345705de6e1068863c
Costin Sin <56958521+costinsin@users.noreply.github.com>
2024-01-15 13:44:26 +0200

    Add dummy class (#3)

---
+++ backend.ts
@@ -0,0 +1,11 @@
+import { GenezioDeploy /*, GenezioMethod */ } from "@genezio/types";
+
+@GenezioDeploy()
+export class BackendService {
+  constructor() {}
+
+  //   @GenezioMethod()
+  //   async hello() {
+  //     return "Hello World";
+  //   }
+}
